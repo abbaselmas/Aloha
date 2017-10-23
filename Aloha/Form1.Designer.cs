3f591c1b1f4d1e7b528029b6372177a832971b25
Abbas Elmas <abbaselmas@gmail.com>
2017-10-23 15:17:22 +0300

    restore github repos

---
+++ Aloha/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Aloha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.g = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.n = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.calc = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.throughput = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.idleResult = new System.Windows.Forms.Label();
+            this.collusionResult = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // g
+            // 
+            this.g.Location = new System.Drawing.Point(6, 19);
+            this.g.Name = "g";
+            this.g.Size = new System.Drawing.Size(100, 20);
+            this.g.TabIndex = 0;
+            this.g.Text = "0,5";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.n);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.g);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(134, 80);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "inputs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "N";
+            // 
+            // n
+            // 
+            this.n.Location = new System.Drawing.Point(6, 46);
+            this.n.Name = "n";
+            this.n.Size = new System.Drawing.Size(100, 20);
+            this.n.TabIndex = 2;
+            this.n.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "G";
+            // 
+            // calc
+            // 
+            this.calc.Location = new System.Drawing.Point(12, 98);
+            this.calc.Name = "calc";
+            this.calc.Size = new System.Drawing.Size(134, 23);
+            this.calc.TabIndex = 2;
+            this.calc.Text = "calculate";
+            this.calc.UseVisualStyleBackColor = true;
+            this.calc.Click += new System.EventHandler(this.calc_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.collusionResult);
+            this.groupBox2.Controls.Add(this.idleResult);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.throughput);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(152, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(135, 109);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Probabilities";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Throughput: ";
+            // 
+            // throughput
+            // 
+            this.throughput.AutoSize = true;
+            this.throughput.Location = new System.Drawing.Point(82, 20);
+            this.throughput.Name = "throughput";
+            this.throughput.Size = new System.Drawing.Size(13, 13);
+            this.throughput.TabIndex = 1;
+            this.throughput.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Idle Slot: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Collusion: ";
+            // 
+            // idleResult
+            // 
+            this.idleResult.AutoSize = true;
+            this.idleResult.Location = new System.Drawing.Point(82, 49);
+            this.idleResult.Name = "idleResult";
+            this.idleResult.Size = new System.Drawing.Size(13, 13);
+            this.idleResult.TabIndex = 4;
+            this.idleResult.Text = "0";
+            // 
+            // collusionResult
+            // 
+            this.collusionResult.AutoSize = true;
+            this.collusionResult.Location = new System.Drawing.Point(82, 77);
+            this.collusionResult.Name = "collusionResult";
+            this.collusionResult.Size = new System.Drawing.Size(13, 13);
+            this.collusionResult.TabIndex = 5;
+            this.collusionResult.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 142);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.calc);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Abbas A. ELMAS Aloha";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox g;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox n;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button calc;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label collusionResult;
+        private System.Windows.Forms.Label idleResult;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label throughput;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Aloha/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aloha
+{
+    public partial class Form1 : Form
+    {
+        public const long a = 1000000;
+        public static Random r = new Random();
+        public ArrayList randomNumberList = new ArrayList();
+        public double N, G, p;
+        int transferCount = 0;
+        double idleSlotCount = 0;
+        double succededTransfer = 0;
+        double collusion = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calc_Click(object sender, EventArgs e)
+        {
+            N = Convert.ToDouble(n.Text);
+            G = Convert.ToDouble(g.Text);
+            p = G / N;
+
+            for (int b = 0; b < a; b++)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    randomNumberList.Add(r.NextDouble());
+                }
+
+                for (int y = 0; y < N; y++)
+                {
+                    if (Convert.ToDouble(randomNumberList[y]) < p)
+                    {
+                        transferCount++;
+                    }
+                }
+
+                if (transferCount == 0)
+                {
+                    idleSlotCount++;
+                }
+                else if (transferCount == 1)
+                {
+                    succededTransfer++;
+                }
+                else
+                {
+                    collusion++;
+                }
+
+                randomNumberList.Clear();
+                transferCount = 0;
+
+            }
+            idleSlotCount /= a;
+            succededTransfer /= a;
+            collusion /= a;
+
+            idleResult.Text = idleSlotCount.ToString();
+            throughput.Text = succededTransfer.ToString();
+            collusionResult.Text = collusion.ToString();
+        }
+    }
+}
